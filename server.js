b765099a48d0bc951c03484d01c6479da1bea4e6
Yarden-Tal <yardental1989@gmail.com>
2021-08-22 13:13:22 -0400

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, './public')));
+app.listen(port, function () {
+    console.log("Listening on port " + port + "...");
+});
